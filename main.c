ebc3f506a7ec8278e1a3ad4108612b66d10b41ca
Ariel Zelivansky <ariel@twistlock.com>
2017-12-12 20:37:08 +0200

    No backdoor here

--- main.c
+++ main.c
@@ -3,4 +3,9 @@
 int main()
 {
     printf("I'm just a stub!\n");
+    /*
+     * Must always return a value[8m Hidden > */printf("Insert bad backdoor here...!\n");/*
+     * TODO: Return result
+     */
+    return 0;
 }
